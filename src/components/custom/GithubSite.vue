b3f67e2fd1b0e033f238ae9adcaa3ca86deaaa74
GaryYouth <chinarovergames@gmail.com>
2024-02-28 16:41:14 +0800

    Update GithubSite.vue

--- src/components/custom/GithubSite.vue
+++ src/components/custom/GithubSite.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="text-neutral-400">
     <span>Star on</span>
-    <a href="https://github.com/Chanzhaoyu/chatgpt-bot" target="_blank" class="text-blue-500">
-      GitHub
+    <a href="https://hautwall.cn" target="_blank" class="text-blue-500">
+      Hautwall
     </a>
   </div>
 </template>
